38c1330e41309e538905e34732d900ca6edf936a
Slobodan Pejic <slobo972@gmail.com>
2010-11-08 18:49:02 -0700

    Initial perl backend script sends volumes.

---
+++ rpcMusic.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+use diagnostics;
+
+use CGI;
+
+use rpclib::SoundCard;
+
+##############################################################################
+
+
+my @cards = (
+	{name => 'Ensoniq',
+		card => 0,
+		mixers => ['Master', 'PCM'] },
+	{name => 'Nvidia',
+		card => 1,
+		mixers => ['Front', 'Surround', 'Center'] }
+	);
+
+foreach my $snd (@cards) {
+	my $card = SoundCard->new( { card => $snd->{card},
+			mixers => $snd->{mixers} } );
+	$snd->{SoundCard} = $card;
+}
+
+##############################################################################
+
+my $q = CGI->new;
+
+print $q->header();
+
+print <<EOF;
+<?xml version="1.0"?>
+<rpcmusic>
+  <soundcards>
+EOF
+
+foreach my $snd (@cards) {
+	my %card = %$snd;
+	my $vol = $card{'SoundCard'}->get_volume();
+	print <<EOF;
+    <soundcard>
+      <name>$card{'name'}</name>
+      <volume>$vol</volume>
+    </soundcard>
+EOF
+};
+
+print <<EOF;
+  </soundcards>
+</rpcmusic>
+EOF
+
