1522ca982d4494d4c0dcb30d35ade03ca7098f35
Slobodan Pejic <slobo972@gmail.com>
2010-11-12 18:39:48 -0700

    Setting volume cgi interface.

--- rpcMusic.pl
+++ rpcMusic.pl
@@ -9,7 +9,11 @@ use CGI;
 use rpclib::SoundCard;
 
 ##############################################################################
+# CONFIG
+#
 
+# The allow volume error in a ratio (percent divided by 100).
+my $VOLUME_ERROR = 0.02;
 
 my @cards = (
 	{name => 'Ensoniq',
@@ -20,15 +24,57 @@ my @cards = (
 		mixers => ['Front', 'Surround', 'Center'] }
 	);
 
+##############################################################################
+# Init & helper code
+#
+
+my $q = CGI->new;
+
 foreach my $snd (@cards) {
 	my $card = SoundCard->new( { card => $snd->{card},
 			mixers => $snd->{mixers} } );
 	$snd->{SoundCard} = $card;
 }
 
+sub get_card_by_name
+{
+	my $name = shift;
+	my @gc = grep {$_->{'name'} == $name} @cards;
+	if (scaler(@gc) == 1) {
+		return ($gc[0]);
+	}
+	return (undef);
+}
+
 ##############################################################################
 
-my $q = CGI->new;
+my @send_errors;
+
+my $cmd = $q->param('cmd');
+$cmd = "none" if !defined($cmd);
+
+if ($cmd eq 'set_volume') {
+	my $card = $q->param('card');
+	my $volume = $q->param('volume');
+	my $cardhash = get_card_by_name($card);
+	if (!defined($cardhash)) {
+		push @send_errors, "Trying to set the volume"
+			. ", but card \"$card\" was not found.";
+	}
+	my $soundCard = $cardhash->{'SoundCard'};
+	$soundCard->set_volume($volume);
+	my $rvol = $soundCard->get_volume();
+	if ($volume < $rvol - $VOLUME_ERROR || $volume > $rvol + $VOLUME_ERROR){
+		push @send_errors, "Couldn't set volume on card \"$card\"."
+			. " Requested volume is $volume and"
+			. " new volume is $rvol.";
+	}
+}
+
+
+##############################################################################
+# Printing results
+#
 
 print $q->header('text/xml');
 
@@ -52,6 +98,20 @@ EOF
 
 print <<EOF;
   </soundcards>
+  <errors>
+EOF
+
+foreach my $error (@send_errors) {
+	$error =~ s/&/&amp;/g;
+	$error =~ s/</&lt;/g;
+	$error =~ s/>/&gt;/g;
+	print <<EOF;
+    <error>$error</error>
+EOF
+}
+
+print <<EOF;
+  </errors>
 </rpcmusic>
 EOF
 
