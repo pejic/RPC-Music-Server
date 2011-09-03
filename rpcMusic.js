b2e582738668bab07486cc9e6faaa892966e48cf
Slobodan Pejic <slobo972@gmail.com>
2011-09-02 19:56:32 -0600

    Documenting the javascript functions.

--- rpcMusic.js
+++ rpcMusic.js
@@ -1,4 +1,9 @@
 
+/*
+ * Logs the given message if the logging textarea is present.
+ *
+ * \param err string containing the error message.
+ */
 function logdbg( err )
 {
   var dbglog = document.getElementById( "dbglog" );
@@ -7,6 +12,9 @@ function logdbg( err )
   }
 }
 
+/*
+ * Clears the error log.
+ */
 function logclear()
 {
   var dbglog = document.getElementById( "dbglog" );
@@ -15,11 +23,22 @@ function logclear()
   }
 }
 
+/*
+ * Same as logdbg but adds a newline at the end.
+ *
+ * \param err string containing the error message.
+ */
 function logdbgln( err )
 {
   logdbg( err + '\n' );
 }
 
+/*
+ * Rounds x to d decimal places.
+ *
+ * \param x the number to round.
+ * \param d the number of decimal places to keep.
+ */
 function round_to( x, d )
 {
   var tens = Math.pow( 10, d );
@@ -27,6 +46,14 @@ function round_to( x, d )
   return( rounded );
 }
 
+/*
+ * Traverses an XML tree recursively printing it to the debug log.
+ *
+ * \param node the DOM node to print from.
+ * \param spaces a string containing the spaces that will be added to the
+ *        beginning of each printed line.  This makes indenting the XML tree
+ *        possible.
+ */
 function traverseXML( node, spaces )
 {
   logdbgln( spaces + "<" + node.nodeName + ">" );
@@ -36,6 +63,12 @@ function traverseXML( node, spaces )
   }
 }
 
+/*
+ * Finds all XML nodes that are children of node and have the given tag name.
+ *
+ * \param node the node which's children will be searched.
+ * \param tagName the name of the tag to be matched.
+ */
 function find_childtags( node, tagName )
 {
   var cnodes = node.childNodes;
@@ -50,6 +83,12 @@ function find_childtags( node, tagName )
   return (results) ;
 }
 
+/*
+ * Finds the first child with given tag.
+ *
+ * \param node the node which's children will be searched.
+ * \param tagName the tag name to be matched.
+ */
 function find_childtag( node, tagName )
 {
   var r = find_childtags( node, tagName );
@@ -61,6 +100,11 @@ function find_childtag( node, tagName )
   }
 }
 
+/*
+ * Returns the text from an XML node.
+ *
+ * \param node the node that contains the text node.
+ */
 function get_text_value( node )
 {
   var te = node.childNodes[0];
@@ -72,6 +116,11 @@ function get_text_value( node )
   }
 }
 
+/*
+ * Returns the soundcards from the result of a server info query.
+ *
+ * \param res the result received from the server.
+ */
 function get_soundcards_from_res( res )
 {
   var rpcmusic = find_childtag( res, "rpcmusic" );
@@ -80,22 +129,43 @@ function get_soundcards_from_res( res )
   return (scs);
 }
 
+/*
+ * Returns the text from a child node of the given tag name.
+ *
+ * \param node the node which's children will be searched.
+ * \param tagname the tag name to be matched.
+ */
 function get_textvalue_from( node, tagname )
 {
   var v = find_childtag( node, tagname );
   return( get_text_value( v ) );
 }
 
+/*
+ * Returns the sound card name from a soundcard XML node.
+ *
+ * \param sc an XML node.
+ */
 function get_soundcard_name( sc )
 {
   return( get_textvalue_from( sc, "name" ) );
 }
 
+/*
+ * Returns the sound card volume from a soundcard XML node.
+ *
+ * \param sc an XML node.
+ */
 function get_soundcard_volume( sc )
 {
   return( get_textvalue_from( sc, "volume" ) );
 }
 
+/*
+ * Returns all music players from a result of a server info query.
+ *
+ * \param res the result received from the server.
+ */
 function get_players_from_res( res )
 {
   var rpcmusic = find_childtag( res, "rpcmusic" );
@@ -104,21 +174,41 @@ function get_players_from_res( res )
   return (ps);
 }
 
+/*
+ * Returns the music player's name.
+ *
+ * \param player the XML node representing a music player.
+ */
 function get_player_playerName( player )
 {
   return( get_textvalue_from( player, "playerName" ) );
 }
 
+/*
+ * Returns the music player's current track's artist.
+ *
+ * \param player the XML node representing a music player.
+ */
 function get_player_artist( player )
 {
   return( get_textvalue_from( player, "artist" ) );
 }
 
+/*
+ * Returns the music player's current track's title.
+ *
+ * \param player the XML node representing a music player.
+ */
 function get_player_title( player )
 {
   return( get_textvalue_from( player, "title" ) );
 }
 
+/*
+ * Returns the first child node that is not a text node.
+ *
+ * \param node the XML node which's children will be searched. 
+ */
 function first_non_text( node )
 {
   var chs = node.childNodes;
@@ -131,6 +221,11 @@ function first_non_text( node )
   return (null);
 }
 
+/*
+ * Returns the next sibling of node that is not a text node.
+ *
+ * \param node the XML node for which the next sibling will be found.
+ */
 function next_non_text( node )
 {
   var sib = node.nextSibling;
@@ -143,6 +238,14 @@ function next_non_text( node )
   return (null);
 }
 
+/*
+ * Constructor for a new volume control widget.  The widget is appended to
+ * pElem.  The name member is set to name.
+ *
+ * \param pElem the parent element.
+ * \param name the name of the volume control.  This should be a name received
+ *        from the server.
+ */
 function VolumeControl( pElem, name )
 {
   this.root = document.createElement("div");
@@ -154,17 +257,36 @@ function VolumeControl( pElem, name )
   this.root.appendChild(this._nameDiv);
   this._nameDiv.appendChild(this._nameText);
   this.name = name;
-  this._control = new ButtonArray( this.root, name + "_vol", 0, 100, 11 );
+  this._control = new ButtonArray( this.root, 0, 100, 11 );
   this._callback = null;
   this._callback_data = null;
+
+  /*
+   * Set the volume to v.
+   *
+   * \param v the new volume in the range [0:1];
+   */
   this.set_volume =
       function (v) {
         this._control.set_value(v*100);
       };
+
+  /*
+   * Returns the current volume in the range [0:1].
+   */
   this.get_volume =
       function () {
         this._control.get_value()/100;
       };
+
+  /*
+   * Set the function that will be called when the volume changes.
+   *
+   * \param cb the callback function that takes two parameters. The first
+   *        parameter is the volume, and the second is the data parameter you
+   *        pass to set_callback.
+   * \param data a parameter that is passed to your callback function.
+   */
   this.set_callback =
       function (cb, data) {
         if (cb == null || (cb != null && typeof(cb) == 'function')) {
@@ -172,6 +294,7 @@ function VolumeControl( pElem, name )
           this._callback_data = data;
         }
       };
+
   this._proxy_callback =
       function (i, this2) {
         if (this2._callback != null) {
@@ -182,7 +305,10 @@ function VolumeControl( pElem, name )
 }
 
 /*
- * Pre: elem must be unattached to a parent node.
+ * Wraps the given element in a div, and sets the div's class attribute.
+ *
+ * \param elem an element that is already attached in the DOM somewhere.
+ * \param className the class that the new div's class attribute is set to.
  */
 function wrap_in_div( elem, className )
 {
@@ -192,6 +318,12 @@ function wrap_in_div( elem, className )
   return (dive);
 }
 
+/*
+ * Constructor for a widget that controls the playback of a music player.
+ *
+ * \param pElem the parent element to which the player widget will be attached
+ *        to.
+ */
 function PlayerControl( pElem )
 {
   this.root = document.createElement( "div" );
@@ -214,6 +346,11 @@ function PlayerControl( pElem )
     tickSpeed = 1;
   }
   tickSpeed = Math.round(tickSpeed);
+
+  /*
+   * Function that moves the song artist and title back and forth when the
+   * whole text cannot fit.
+   */
   function tickSongdiv() {
     var iw = songdiv.scrollWidth;
     var w = songdiv.offsetWidth;
@@ -294,8 +431,18 @@ function PlayerControl( pElem )
       return (this._title.nodeValue);
     };
 
+  /*
+   * Signals that callbacks can listen for.
+   */
   var signal_types = ['previous', 'play', 'next'];
 
+  /*
+   * Sets the callback for a given signal.
+   *
+   * \param signal the signal that the callback listens for.
+   * \param cf your callback function.
+   * \param data the data to be passed to your callback function.
+   */
   this.set_callback =
     function (signal, cf, data) {
       // TODO : check that signal is in signal_types
@@ -304,6 +451,9 @@ function PlayerControl( pElem )
     };
 
 
+  /*
+   * Here we set up proxy callback functions for each type of signal.
+   */
   this._callback_proxies = {};
   var this2 = this;
   this._create_callback_proxy =
@@ -320,6 +470,11 @@ function PlayerControl( pElem )
     var stype = signal_types[stypei];
     this._create_callback_proxy(stype);
   }
+
+  /*
+   * Here we create buttons for each signal, and connect them to the proxy
+   * callback functions.
+   */
   var buttonRow = document.createElement("div");
   buttonRow.className = "buttonRow";
   for (var stypei in signal_types) {
@@ -334,7 +489,16 @@ function PlayerControl( pElem )
 
 }
 
-function ButtonArray( pElem, name, min, max, num )
+/*
+ * Constructor for an array of buttons that are numbered.  This is intended for
+ * the volume control widget.
+ *
+ * \param pElem the node to which the new button array will be attached.
+ * \param min the minumum value of the range for this widget.
+ * \param max the maximum value of the range for this widget.
+ * \param num the number of buttons to create.
+ */
+function ButtonArray( pElem, min, max, num )
 {
   var i;
 
@@ -361,6 +525,11 @@ function ButtonArray( pElem, name, min, max, num )
 
   this._currentI = -1;
 
+  /*
+   * Given the index returns the value at that index.
+   *
+   * \param i the index of the button.
+   */
   this.value_at_i = 
       function (i) {
         var t = (i/(this.num-1));
@@ -368,6 +537,11 @@ function ButtonArray( pElem, name, min, max, num )
         return( value );
       };
 
+  /*
+   * Given the value within the range returns the button index.
+   *
+   * \param v the value of the button.
+   */
   this.i_for_value =
       function (v) {
         var t = (v - this.min)/(this.max-this.min);
@@ -381,6 +555,12 @@ function ButtonArray( pElem, name, min, max, num )
         return( n );
       };
 
+  /*
+   * Sets the value of the button array.  This switches the selected button if
+   * the button index changes.
+   *
+   * \param v value to be set.
+   */
   this.set_value =
       function (v) {
         var i = this.i_for_value( v );
@@ -397,6 +577,14 @@ function ButtonArray( pElem, name, min, max, num )
         this.set_value( v );
       };
 
+  /*
+   * Sets a callback that is called when the selected button changes.
+   *
+   * \param cb your callback function that takes two parameters: the index of
+   *        the selected button, and the data variable you pass to
+   *        set_callback.
+   * \param data a variable to be passed to your callback function.
+   */
   this.set_callback =
       function (cb, data) {
         if (cb == null || (cb != null && typeof(cb) == 'function')) {
@@ -410,12 +598,18 @@ function ButtonArray( pElem, name, min, max, num )
         }
       };
 
+  /*
+   * Removes itself from the DOM.
+   */
   this.destroy = 
       function () {
         this.root.parentNode.removeChild(this.root);
         this.root = null;
       };
 
+  /*
+   * Returns the value of the currently selected button.
+   */
   this.get_value =
       function () {
         if (this.currentI >= 0 && this.currentI < this.num) {
@@ -449,6 +643,14 @@ function ButtonArray( pElem, name, min, max, num )
   }
 }
 
+
+/*
+ * Constructor for all the controls on the page.  The page is built dynamically
+ * from the query to the server.  The server determines which music player
+ * controls are show, and which volume controls are show.
+ *
+ * \param pElem the parent element to which the controls will be appended.
+ */
 function Controls( pElem )
 {
   this.root = document.createElement("div");
@@ -458,6 +660,9 @@ function Controls( pElem )
   this.volumes = new Array(); // will hold the volume widgets
   this.players = new Array(); // will hold the player widgets
 
+  /*
+   * Handles the server's data, to build or update the UI.
+   */
   this._on_info =
       function (req) {
         if (req.readyState == 4) {
